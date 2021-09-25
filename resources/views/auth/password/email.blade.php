48a9d838bfe89762d94716c60b73daf5f2a38c9e
vagrant <vagrant@homestead>
2021-09-25 05:33:25 +0000

    重置密码

--- resources/views/auth/password/email.blade.php
+++ resources/views/auth/password/email.blade.php
@@ -5,12 +5,25 @@
   <div class="col-md-8 offset-md-2">
     <div class="card">
       <div class="card-header"><h5>重置密码</h5></div>
+
       <div class="card-body">
+        {{-- session('status')不知道有什么用 --}}
+        @if (session('status'))
+          <div class="alert alert-success">
+            {{ session('status') }}
+          </div>
+        @endif
+
         <form action="{{ route('password.email') }}" method="post">
           {{ csrf_field() }}
           <div class="form-group">
             <label for="email">邮箱地址：</label>
             <input type="email" id="email" name="email" class="form-control" value="{{ old('email') }}">
+            @if ($errors->has('email'))
+              <span class="form-text" style="color: red">
+                <strong>{{ $errors->first('email') }}</strong>
+              </span>
+            @endif
           </div>
           <div class="form-group">
             <button type="submit" class="btn btn-primary">发送重置密码邮件</button>
--- routes/web.php
+++ routes/web.php
@@ -67,3 +67,4 @@ Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('conf
 Route::get('password/request', 'PasswordController@showRequestForm')->name('password.request');
 Route::post('password/email', 'PasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'PasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'PasswordController@reset')->name('password.update');
