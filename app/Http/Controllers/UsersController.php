1141c9057d1320b596bc1e7c1eeec2fd1519700a
katrigeng <453875798@qq.com>
2021-10-17 18:14:10 +0000

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -36,8 +36,9 @@ class UsersController extends Controller
 
     public function show(User $user){
         // $this->authorize('update', $user);
+        $statuses = $user->statuses()->orderBy('created_at','desc')->paginate(10);
 
-        return view('users.show', compact('user'));
+        return view('users.show', compact('user', 'statuses'));
     }
 
     public function store(Request $request){
