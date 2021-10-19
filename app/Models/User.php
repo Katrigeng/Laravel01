7880312dfa43a3163749bf868b4f13784c0f7545
katrigeng <453875798@qq.com>
2021-10-19 17:09:47 +0000

    粉丝列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,4 +67,36 @@ class User extends Authenticatable
     public function feed(){
         return $this->statuses()->orderBy('created_at', 'desc');
     }
+
+    //用户的粉丝列表
+    public function followers(){
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    //用户的关注列表
+    public function followings(){
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    //关注
+    public function follow($user_ids){
+        if(!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        return $this->followings()->sync($user_ids, false);
+    }
+
+    //取消关注
+    public function unfollow($user_ids){
+        if(!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        return $this->followings()->detach($user_ids);
+    }
+
+    //当前用户 是否关注 某某用户
+    public function isFollowing($user_ids){
+        return $this->followings()->contains($user_ids);
+    }
+
 }
